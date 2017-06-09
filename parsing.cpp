b2019f80de3cb22bb4f2d423906566d6df6d268f
Mark A. Caprio <mcaprio-nd-edu>
2017-06-09 19:41:05 -0400

    parsing: Move into mcutils namespace

--- parsing.cpp
+++ parsing.cpp
@@ -10,38 +10,45 @@
 
 #include <cstdlib>
 
-void StreamCheck(bool success, const std::string& filename, const std::string& message)
+namespace mcutils
 {
-  if (!success)
-    {
-      std::cerr << std::endl;
-      std::cerr << "File access failure: " << filename << std::endl;
-      std::cerr << message << std::endl;
-      std::exit(EXIT_FAILURE);
-    }
-}
+  ////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////
 
-void OpenCheck(bool success, const std::string& filename)
-{
-  // if (!success)
-  //   {
-  //     std::cerr << std::endl;
-  //     std::cerr << "Failed to open file: " << filename << std::endl;
-  //     std::exit(EXIT_FAILURE);
-  //   }
-  StreamCheck(success,filename, "Failed to open file");
-}
+  void StreamCheck(bool success, const std::string& filename, const std::string& message)
+  {
+    if (!success)
+      {
+        std::cerr << std::endl;
+        std::cerr << "File access failure: " << filename << std::endl;
+        std::cerr << message << std::endl;
+        std::exit(EXIT_FAILURE);
+      }
+  }
 
-void ParsingError(int line_count, const std::string& line, const std::string& message)
-{
-  std::cerr << std::endl;
-  std::cerr << message << std::endl;
-  std::cerr << "Input line " << line_count << ": " << line  << std::endl;
-  std::exit(EXIT_FAILURE);
-}
+  void OpenCheck(bool success, const std::string& filename)
+  {
+    // if (!success)
+    //   {
+    //     std::cerr << std::endl;
+    //     std::cerr << "Failed to open file: " << filename << std::endl;
+    //     std::exit(EXIT_FAILURE);
+    //   }
+    StreamCheck(success,filename, "Failed to open file");
+  }
 
-void ParsingCheck(std::istringstream& line_stream, int line_count, const std::string& line)
-{
-  if (!line_stream)
-    ParsingError(line_count,line,"Failed parsing line (missing or incorrect arguments)");
-}
+  void ParsingError(int line_count, const std::string& line, const std::string& message)
+  {
+    std::cerr << std::endl;
+    std::cerr << message << std::endl;
+    std::cerr << "Input line " << line_count << ": " << line  << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  void ParsingCheck(std::istringstream& line_stream, int line_count, const std::string& line)
+  {
+    if (!line_stream)
+      ParsingError(line_count,line,"Failed parsing line (missing or incorrect arguments)");
+  }
+
+}  // namespace
--- parsing.h
+++ parsing.h
@@ -1,5 +1,5 @@
 /****************************************************************
-  parsing.cpp
+  parsing.h
 
   Generic input parsing and error message utilities.
 `
@@ -13,6 +13,9 @@
     - OpenCheck: Deprecated in favor of StreamCheck.
     - StreamCheck: Add user-provided error message.
     - ParsingError: Reorder arguments for consistency.
+  6/9/17 (mac):
+    - Remove deprecated function OpenCheck.
+    - Move into namespace mcutils.
 
 ****************************************************************/
 
@@ -23,86 +26,84 @@
 #include <sstream>
 #include <string>
 
-void StreamCheck(bool success, const std::string& filename, const std::string& message);
-// Check stream status and, if in failure state, terminate with error
-// message indicating stream access failure.
-//
-// Arguments:
-//   success (bool) : access status
-//   filename (string) : file name to use in error messasge
-//   message (string) : general error message
-//
-// The success parameter was meant to be used with automatic
-// conversion from stream to bool, but explicit conversion
-// bool(stream) may be needed (e.g., C++11 under gcc 5) and is thus
-// more portable.
-//
-// Example:
-//   StreamCheck(bool(in_stream),in_stream_name,"Failed to open file");
-//   StreamCheck(bool(in_stream),in_stream_name,"Failure while writing header");
+namespace mcutils
+{
+  ////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////
 
-void OpenCheck(bool success, const std::string& filename);
-// Check stream status and, if in failure state, terminate with error
-// message indicating stream open failure.
-//
-// DEPRECATED in favor of StreamCheck with appropriate error message:
-//
-//   StreamCheck(bool(in_stream),in_stream_name,"Failed to open input file");
-//
-// Arguments:
-//   success (bool) : access status
-//   filename (string) : file name to use in error messasge
-//
-// The success parameter was meant to be used with automatic
-// conversion from stream to bool, but explicit conversion
-// bool(stream) may be needed (e.g., C++11 under gcc 5) and is thus
-// more portable.
-//
-// Example:
-//   OpenCheck(bool(in_stream),in_stream_name);
 
-void ParsingError(int line_count, const std::string& line, const std::string& message);
-// Generate error message indicating line of input.
-//
-// Limitations: Would ideally also support arguments to give filename
-// and optional supplementary information on expected content.
-//
-// Arguments:
-//   line_count (int) : line count for error message
-//   line (string) : line text for error message
-//   message (string) : general error message
+  void StreamCheck(bool success, const std::string& filename, const std::string& message);
+  // Check stream status and, if in failure state, terminate with error
+  // message indicating stream access failure.
+  //
+  // Arguments:
+  //   success (bool) : access status
+  //   filename (string) : file name to use in error messasge
+  //   message (string) : general error message
+  //
+  // The success parameter was meant to be used with automatic
+  // conversion from stream to bool, but explicit conversion
+  // bool(stream) may be needed (e.g., C++11 under gcc 5) and is thus
+  // more portable.
+  //
+  // Example:
+  //   StreamCheck(bool(in_stream),in_stream_name,"Failed to open file");
+  //   StreamCheck(bool(in_stream),in_stream_name,"Failure while writing header");
 
-void ParsingCheck(std::istringstream& line_stream, int line_count, const std::string& line);
-// Provide error message upon stream extraction failure for line of input.
-//
-// Limitations: Would ideally also support arguments to give filename
-// and optional supplementary information on expected content.
-//
-// Arguments:
-//   line_stream (istringstream) : string stream from which parsing was attempted
-//   line_count (int) : line count for error message
-//   line (string) : line text for error message
-//
-// Example:
-//
-//    // scan input file
-//    std::string line;
-//    int line_count = 0;
-//    while ( std::getline(in_stream, line) )
-//    {
-//      // count line
-//      ++line_count;
-//
-//      // set up for parsing
-//      std::istringstream line_stream(line);
-//
-//      // parse line
-//      int a, b, ...;
-//      line_stream >> a >> b >> ...;
-//      ParsingCheck(line_stream, line_count, line);
-//  
-//      // do stuff with input
-//      ...
-//    }
+  void ParsingError(int line_count, const std::string& line, const std::string& message);
+  // Generate error message indicating line of input.
+  //
+  // Limitations: Would ideally also support arguments to give filename
+  // and optional supplementary information on expected content.
+  //
+  // Arguments:
+  //   line_count (int) : line count for error message
+  //   line (string) : line text for error message
+  //   message (string) : general error message
+
+
+  void ParsingCheck(std::istringstream& line_stream, int line_count, const std::string& line);
+  // Provide error message upon stream extraction failure for line of input.
+  //
+  // Limitations: Would ideally also support arguments to give filename
+  // and optional supplementary information on expected content.
+  //
+  // Arguments:
+  //   line_stream (istringstream) : string stream from which parsing was attempted
+  //   line_count (int) : line count for error message
+  //   line (string) : line text for error message
+  //
+  // Example:
+  //
+  //    // scan input file
+  //    std::string line;
+  //    int line_count = 0;
+  //    while ( std::getline(in_stream, line) )
+  //    {
+  //      // count line
+  //      ++line_count;
+  //
+  //      // set up for parsing
+  //      std::istringstream line_stream(line);
+  //
+  //      // parse line
+  //      int a, b, ...;
+  //      line_stream >> a >> b >> ...;
+  //      ParsingCheck(line_stream, line_count, line);
+  //  
+  //      // do stuff with input
+  //      ...
+  //    }
+
+}  // namespace
+
+
+// legacy support for global definitions
+
+#ifdef MCUTILS_ALLOW_LEGACY_GLOBAL
+using mcutils::StreamCheck;
+using mcutils::ParsingError;
+using mcutils::ParsingCheck;
+#endif
 
 #endif
