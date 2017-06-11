597b1456becb3fbba66657c363b8795550e667a1
Mark A. Caprio <mcaprio-nd-edu>
2017-06-11 16:03:22 -0400

    io: Add error message to VerifyBinary

--- io.h
+++ io.h
@@ -14,6 +14,7 @@
 #define MCUTILS_IO_H_
 
 #include <iostream>
+#include <string>
 
 namespace mcutils
 {
@@ -21,29 +22,76 @@ namespace mcutils
   ////////////////////////////////////////////////////////////////
 
 
-  template <typename T>
-  void WriteBinary(std::ostream& os, T data)
-  // Write binary data item to stream.
-  {
-    os.write(reinterpret_cast<const char*> (&data),sizeof(data));
-  }
+  template <typename tDataType>
+    void WriteBinary(std::ostream& os, tDataType data)
+    // Write binary data item to stream.
+    //
+    // Note that, if the template parameter is omitted, the data type
+    // of the value given for data will determine the output type, but
+    // explicitly giving the template parameter casts the data to the
+    // given data type tDataType.  Explicitly giving the template
+    // parameter is recommended both to document the data format in
+    // the output file and to avoid any ambiguity of the output type.
+    //
+    // Arguments:
+    //   os (input): binary stream for output
+    //   data (input): data value to output
+    //
+    // Ex:
+    //   mcutils::WriteBinary<float>(out_stream,value);
 
-  template <typename T>
-  void ReadBinary(std::istream& is, T &data)
-  // Read binary data item from stream.
-  {
-    is.read(reinterpret_cast<char*> (&data),sizeof(data));
-  }
+    {
+      os.write(reinterpret_cast<const char*> (&data),sizeof(data));
+    }
 
-  template <typename T>
-  bool VerifyBinary(std::istream& is, T benchmark_data)
-  // Read binary data item from stream and verify against benchmark
-  // value.
-  {
-    T data;
-    ReadBinary<T>(is,data);
-    return (data==benchmark_data);
-  }
+  template <typename tDataType>
+    void ReadBinary(std::istream& is, tDataType &data)
+    // Read binary data item from stream.
+    //
+    // Arguments:
+    //   is (input): binary stream for input
+    //   data (output): data value read from stream
+    //
+    // Ex:
+    //   mcutils::ReadBinary<float>(in_stream,value);
+    {
+      is.read(reinterpret_cast<char*> (&data),sizeof(data));
+    }
+
+  template <typename tDataType>
+    void VerifyBinary(
+        std::istream& is, tDataType benchmark_data,
+        const std::string& message, const std::string& item_name
+      )
+    // Read binary data item from stream and verify against benchmark
+    // value.
+    //
+    // Arguments:
+    //   is (input): binary stream for input
+    //   benchmark_data (input): expected data value
+    //   message (input): text for general error message describing context of error
+    //   item_name (input): text describing meaning of value being checked
+    //
+    // Ex:
+    //   mcutils::VerifyBinary<int>(
+    //       in_stream,bytes,
+    //       "Encountered unexpected value in data file","record delimiter"
+    //     );
+
+    {
+      tDataType data;
+      ReadBinary<tDataType>(is,data);
+      if (data!=benchmark_data)
+        {
+          std::cerr << std::endl;
+          std::cerr << message << std::endl;
+          std::cerr
+            << "Encountered input value " << data << " for " << item_name
+            << " when expecting " << benchmark_data << std::endl;
+          std::exit(EXIT_FAILURE);
+        }
+
+    }
 
 }  // namespace
 
