60e4a82dfff7cb2141026cdb366db53eff1bc566
Patrick Fasano <patrick@patrickfasano.com>
2017-08-11 16:49:19 -0400

    deprecated: Add convenience macros for marking entities as deprecated

---
+++ deprecated.h
@@ -0,0 +1,26 @@
+/****************************************************************
+  deprecated.h
+
+  Defines arithmetic shorthands.
+
+  Patrick J. Fasano
+  University of Notre Dame
+
+  - 08/11/17 (pjf): Created.
+
+****************************************************************/
+#ifndef MCUTILS_DEPRECATED_H_
+#define MCUTILS_DEPRECATED_H_
+
+// borrowed from http://en.cppreference.com/w/cpp/experimental/feature_test
+#ifdef __has_cpp_attribute                      // Check if __has_cpp_attribute is present
+#  if __has_cpp_attribute(deprecated)           // Check for an attribute
+#    define DEPRECATED(msg) [[deprecated(msg)]]
+#  endif
+#endif
+#ifndef DEPRECATED
+#    define DEPRECATED(msg)
+#endif
+
+
+#endif  // MCUTILS_DEPRECATED_H_
