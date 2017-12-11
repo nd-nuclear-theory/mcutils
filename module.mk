7eb59b4cf76a3fbaef25cfe58f5b453b493141ae
Mark A. Caprio <mcaprio-nd-edu>
2017-12-10 19:26:00 -0500

    module.mk: Cleanup

--- module.mk
+++ module.mk
@@ -4,13 +4,15 @@ $(eval $(begin-module))
 # unit definitions
 ################################################################
 
+# units
 module_units_h += arithmetic vector_tuple memoizer profiling eigen gsl io
 module_units_cpp-h += parsing
-module_units_f += 
+
+# programs
 module_programs_cpp +=
+
+# test programs
 module_programs_cpp_test += arithmetic_test eigen_test gsl_test io_test memoizer_test profiling_test vector_tuple_test
-module_programs_f +=
-module_generated +=
 
 ################################################################
 # library creation flag
