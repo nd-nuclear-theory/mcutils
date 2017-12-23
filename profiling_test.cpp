de442ce4b435b3db14fdde9714c67f2311c4b47d
Mark A. Caprio <mcaprio-nd-edu>
2017-12-23 16:56:10 -0600

    profiling: Add to test code

--- profiling_test.cpp
+++ profiling_test.cpp
@@ -12,15 +12,14 @@
 
 void DoDelay()
 {
-  const int iterations = 10000000;
+  const int iterations = 100000000;
   float x=0;
   for (int i=0; i<iterations; ++i) {x+=0.1;};
   std::cout << x << std::endl;  // needed to avoid loop being optimized away!
 }
 
-int main(int argc, char **argv)
+void TestTimer()
 {
-  
   // legacy clock timer
   std::cout << "Timer" << std::endl;
 
@@ -35,7 +34,10 @@ int main(int argc, char **argv)
   std::cout << "(Time: " << timer.ElapsedTime() << ")" << std::endl;
 
   std::cout << std::endl;
+}
 
+void TestSteadyTimer()
+{
   // steady_clock timer
   std::cout << "SteadyTimer" << std::endl;
 
@@ -49,6 +51,34 @@ int main(int argc, char **argv)
   DoDelay();
   // TEST OMITTING Stop: steady_timer.Stop();
   std::cout << "(Time: " << steady_timer.ElapsedTime() << ")" << std::endl;
+
+  std::cout << std::endl;
+}
+
+void CompareTimers()
+{
+  // steady_clock timer
+  std::cout << "Compare" << std::endl;
+
+  mcutils::Timer timer;
+  mcutils::SteadyTimer steady_timer;
+  timer.Start();
+  steady_timer.Start();
+  DoDelay();
+  timer.Stop();
+  steady_timer.Stop();
+  std::cout << "(Time: timer " << timer.ElapsedTime() << ", steady_timer " << steady_timer.ElapsedTime() << ")" << std::endl;
+
+  std::cout << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+  
+  TestTimer();
+  TestSteadyTimer();
+  CompareTimers();
+
 
   // termination
   std::exit(EXIT_SUCCESS);
