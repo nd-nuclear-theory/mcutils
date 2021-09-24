f18bfe08396beea4b1e026daf6cb6deff6d25fac
Patrick Fasano <patrick@patrickfasano.com>
2021-09-24 18:17:56 -0400

    arithmetic: Make functions constexpr

--- arithmetic.h
+++ arithmetic.h
@@ -1,15 +1,16 @@
 /****************************************************************
-  arithmetic.h                       
+  arithmetic.h
 
   Defines arithmetic shorthands.
-                                  
-  Created by Mark A. Caprio on 2/17/11.   
+
+  Created by Mark A. Caprio on 2/17/11.
   Drawing upon libmc/mcutils C code.
 
   - 2/23/11 (mac): Rename from mc_arithmetic to arithmetic.
   - 3/26/17 (mac): Import KroneckerDelta from spncci's utilities (aem).
   - 6/9/17 (mac): Move into namespace mcutils.
   - 11/5/17 (mac): Add UnitStep based on acm (wcl).
+  - 09/24/21 (pjf): Make functions constexpr.
 
 ****************************************************************/
 
@@ -17,7 +18,7 @@
 #define MCUTILS_ARITHMETIC_H_
 
 // ONLYIF(cond,x) evaluates and returns x only if cond is true
-#define ONLYIF(cond,x) ( (cond) ? (x) : 0)    
+#define ONLYIF(cond,x) ( (cond) ? (x) : 0)
 
 namespace mcutils
 {
@@ -25,8 +26,8 @@ namespace mcutils
   ////////////////////////////////////////////////////////////////
 
   template <typename T>
-    inline
-    T sqr(const T& x) 
+    inline constexpr
+    T sqr(const T& x)
     // sqr(x) returns the arithmetic square of x by self-multiplication
     //   Note: Use of inline template avoids double evaluation of x which
     //   would occur in a macro implementation.
@@ -35,23 +36,23 @@ namespace mcutils
     }
 
   template <typename T>
-    inline
+    inline constexpr
     int KroneckerDelta(const T& x, const T& y)
     // Calculate Kronecker delta of variables x and y.
     //
     // That is, returns 1 if x==y, 0 otherwise.
     {
-      return int(x==y);  // Is int(true) guaranteed to be 1?
+      return (x==y) ? 1 : 0;
     }
 
   template <typename T>
-    inline
+    inline constexpr
     int UnitStep(const T& x)
     // Calculate unit step function.
     //
     // That is, returns 1 if x>=0, 0 otherwise.
     {
-      return int(x>=0);  // Is int(true) guaranteed to be 1?
+      return (x>=0) ? 1 : 0;
     }
 
 
